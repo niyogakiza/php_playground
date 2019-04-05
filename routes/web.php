e9739b53c9d0e940b5df62b3d4fea0cef4d834c9
Aimable <aimable.niyogakiza@drpgroup.com>
2019-04-05 18:53:11 +0100

    arrays

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,29 @@
 */
 
 Route::get('/', function () {
-    echo 'hello ';
+    return view('welcome');
+});
+
+
+Route::get('/arrays', function () {
+    $names = ['john', 'jeff', 'Thomas'];
+
+    foreach ($names as $name) {
+        echo $name . ', ';
+    }
+});
+
+Route::get('/variables', function () {
+
+    /* variables and string */
+    $myName = 'Jamie';
+    $anotherName = 'John';
+    $greeting = 'Ola';
+
+    echo "Hello $myName"; // nesting in double quotes only when is a string
+    echo 'Hello,' .$anotherName;
+
+    echo $greeting . ','. $anotherName;
+
+
 });
